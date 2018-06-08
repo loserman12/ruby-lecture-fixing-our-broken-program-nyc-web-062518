9fdb2d999652cbbb73b68809a0701f23b6b893b3
Your Name <you@example.com>
2018-06-08 18:55:17 -0400

    done

--- lib/a_broken_program.rb
+++ lib/a_broken_program.rb
@@ -15,7 +15,5 @@ puts "First come, first served."
 puts "Programs are composed of basically three things:"
 
 puts "A language's keywords, like 'if' or 'end' (approximately 43)."
-puts "Literal pieces of data like this very sentence (or String)."
-puts "Finally, barewords, or variables, that are set equal to things."
-
-puts "Anything that isn't one of those will cause an error
+puts "Literal pieces of data l
+puts "Anything that isn't one of thos
